66fe0f3d8d8f4233dc618ced07f2a10b94104d7e
iky1735 <66539264+iky1735@users.noreply.github.com>
2020-07-01 00:10:49 -0600

    Add files via upload

---
+++ ChatLab/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace ChatLab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbxIP1 = new System.Windows.Forms.TextBox();
+            this.txtbxIP2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbxIP3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbxIP4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtbxUserName = new System.Windows.Forms.TextBox();
+            this.txtbxMessage = new System.Windows.Forms.TextBox();
+            this.bttnSend = new System.Windows.Forms.Button();
+            this.friendListView = new System.Windows.Forms.ListView();
+            this.messageBoxMain = new System.Windows.Forms.RichTextBox();
+            this.nyName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(74, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".";
+            // 
+            // txtbxIP1
+            // 
+            this.txtbxIP1.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbxIP1.Location = new System.Drawing.Point(12, 82);
+            this.txtbxIP1.MaxLength = 3;
+            this.txtbxIP1.Name = "txtbxIP1";
+            this.txtbxIP1.Size = new System.Drawing.Size(56, 20);
+            this.txtbxIP1.TabIndex = 1;
+            // 
+            // txtbxIP2
+            // 
+            this.txtbxIP2.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbxIP2.Location = new System.Drawing.Point(98, 82);
+            this.txtbxIP2.MaxLength = 3;
+            this.txtbxIP2.Name = "txtbxIP2";
+            this.txtbxIP2.Size = new System.Drawing.Size(56, 20);
+            this.txtbxIP2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = ".";
+            // 
+            // txtbxIP3
+            // 
+            this.txtbxIP3.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbxIP3.Location = new System.Drawing.Point(184, 82);
+            this.txtbxIP3.MaxLength = 3;
+            this.txtbxIP3.Name = "txtbxIP3";
+            this.txtbxIP3.Size = new System.Drawing.Size(56, 20);
+            this.txtbxIP3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(246, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = ".";
+            // 
+            // txtbxIP4
+            // 
+            this.txtbxIP4.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbxIP4.Location = new System.Drawing.Point(270, 82);
+            this.txtbxIP4.MaxLength = 3;
+            this.txtbxIP4.Name = "txtbxIP4";
+            this.txtbxIP4.Size = new System.Drawing.Size(56, 20);
+            this.txtbxIP4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IP Address";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "UserName";
+            // 
+            // txtbxUserName
+            // 
+            this.txtbxUserName.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbxUserName.Location = new System.Drawing.Point(12, 32);
+            this.txtbxUserName.Name = "txtbxUserName";
+            this.txtbxUserName.Size = new System.Drawing.Size(314, 20);
+            this.txtbxUserName.TabIndex = 9;
+            // 
+            // txtbxMessage
+            // 
+            this.txtbxMessage.BackColor = System.Drawing.SystemColors.Info;
+            this.txtbxMessage.Location = new System.Drawing.Point(12, 194);
+            this.txtbxMessage.MaxLength = 250;
+            this.txtbxMessage.Multiline = true;
+            this.txtbxMessage.Name = "txtbxMessage";
+            this.txtbxMessage.Size = new System.Drawing.Size(791, 43);
+            this.txtbxMessage.TabIndex = 10;
+            // 
+            // bttnSend
+            // 
+            this.bttnSend.Location = new System.Drawing.Point(809, 203);
+            this.bttnSend.Name = "bttnSend";
+            this.bttnSend.Size = new System.Drawing.Size(84, 23);
+            this.bttnSend.TabIndex = 11;
+            this.bttnSend.Text = "Send";
+            this.bttnSend.UseVisualStyleBackColor = true;
+            this.bttnSend.Click += new System.EventHandler(this.BttnSend_Click);
+            // 
+            // friendListView
+            // 
+            this.friendListView.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.friendListView.HideSelection = false;
+            this.friendListView.Location = new System.Drawing.Point(375, 32);
+            this.friendListView.Name = "friendListView";
+            this.friendListView.Size = new System.Drawing.Size(518, 156);
+            this.friendListView.TabIndex = 14;
+            this.friendListView.UseCompatibleStateImageBehavior = false;
+            this.friendListView.View = System.Windows.Forms.View.Details;
+            // 
+            // messageBoxMain
+            // 
+            this.messageBoxMain.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.messageBoxMain.Location = new System.Drawing.Point(12, 243);
+            this.messageBoxMain.Name = "messageBoxMain";
+            this.messageBoxMain.Size = new System.Drawing.Size(881, 255);
+            this.messageBoxMain.TabIndex = 15;
+            this.messageBoxMain.Text = "";
+            this.messageBoxMain.TextChanged += new System.EventHandler(this.MessageBoxMain_TextChanged);
+            // 
+            // nyName
+            // 
+            this.nyName.BackColor = System.Drawing.SystemColors.Info;
+            this.nyName.Location = new System.Drawing.Point(12, 147);
+            this.nyName.Name = "nyName";
+            this.nyName.Size = new System.Drawing.Size(314, 20);
+            this.nyName.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 20);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Contact Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(905, 510);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.nyName);
+            this.Controls.Add(this.messageBoxMain);
+            this.Controls.Add(this.friendListView);
+            this.Controls.Add(this.bttnSend);
+            this.Controls.Add(this.txtbxMessage);
+            this.Controls.Add(this.txtbxUserName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbxIP4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbxIP3);
+            this.Controls.Add(this.txtbxIP2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbxIP1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbxIP1;
+        private System.Windows.Forms.TextBox txtbxIP2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbxIP3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbxIP4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtbxUserName;
+        private System.Windows.Forms.TextBox txtbxMessage;
+        private System.Windows.Forms.Button bttnSend;
+        private System.Windows.Forms.ListView friendListView;
+        private System.Windows.Forms.RichTextBox messageBoxMain;
+        private System.Windows.Forms.TextBox nyName;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ChatLab/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace ChatLab
+{
+    public partial class Form1 : Form
+    {
+        byte[] _messageBytes; //byte array of encoded message
+        List<IPAddress> _friendsList = new List<IPAddress>(); //list of all ip addresses currently on send list
+        readonly List<string> _names = new List<string>();
+        readonly UdpClient client = new UdpClient(1666); //new UdpClient for send/receive functions
+        IPAddress _sendToMe; //IPAddress which will be derived from user inputs
+        
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+
+        //Occurs on form load
+        //generates default inputs, formats columns
+        //and invokes ReceiveMessage so IMs can be instantly received
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //save me some test typing
+            txtbxIP1.Text = "127";
+            txtbxIP2.Text = "0";
+            txtbxIP3.Text = "0";
+            txtbxIP4.Text = "1";
+
+            //format contact list
+            ColumnHeader header = new ColumnHeader
+            {
+                Text = "Name",
+                Width = (friendListView.Width - 10) / 2
+            };
+            friendListView.Columns.Add(header);
+            ColumnHeader header2 = new ColumnHeader
+            {
+                Text = "IP Address",
+                Width = (friendListView.Width - 10) / 2
+            };
+            friendListView.Columns.Add(header2);
+            _friendsList.Add(new IPAddress(new byte[4] { 127, 0, 0, 1 }));
+            _names.Add("Horse");
+            DisplayIPS();
+
+            //ensure IMs can be immediately rcvs
+            _ = ReceiveMessage();
+        }
+
+        //occurs when user presses send button, compiles message from user input and 
+        //sends to ip address from input
+        private void BttnSend_Click(object sender, EventArgs e)
+        {
+            //parse an IPAddress from user inputs
+            _sendToMe = new IPAddress(new byte[4] { byte.Parse(txtbxIP1.Text), byte.Parse(txtbxIP2.Text), byte.Parse(txtbxIP3.Text), byte.Parse(txtbxIP4.Text) });
+
+            //if contact is not in contact list...
+            if (!_friendsList.Contains(_sendToMe) && !_names.Contains(txtbxUserName.Text))
+            {
+                //add a new friend
+                //_friendDic.Add(txtbxUserName.Text, _sendToMe);
+                _friendsList.Add(_sendToMe);
+                _names.Add(txtbxUserName.Text);
+                DisplayIPS();
+            }
+            //generate byte array from user inputs
+            GenerateMessage();
+            //await new send/receives
+            _ = ReceiveMessage();
+            _ = SendMessage();
+        }
+
+        //private void GenerateMessage()
+        //builds a message byte array from user supplied inputs
+        //accepts - nothing
+        //returns - nothing
+        private void GenerateMessage()
+        {
+            string encodeMe; //final string message to be encoded
+            string username; //string component representing username
+            string message; //* * message
+            string timeStamp; //* * timestamp
+
+            //generate new byte array
+            _messageBytes = new byte[20 + 19 + txtbxMessage.Text.Length + 1];
+
+            //ensure user supplies themselves with a username
+            if (nyName.Text == "")
+            {
+                MessageBox.Show("Error: Please enter username");
+                return;
+            }
+
+            //reset message array (redundant)
+            Array.Clear(_messageBytes, 0, _messageBytes.Length);
+
+            //assign strings from user input
+            username = nyName.Text;
+            message = txtbxMessage.Text;
+            timeStamp = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+
+            //pad username for agreed upon formatting
+            if (username.Length < 20)
+            {
+                for (int i = username.Length; i < 20; ++i)
+                {
+                    username += " ";
+                }
+            }
+
+            //sanity checking
+            encodeMe = username + timeStamp + message;
+            Console.WriteLine(username + " : " + username.Length + " chars");
+            Console.WriteLine(timeStamp + " : " + timeStamp.Length + " chars");
+            Console.WriteLine(message + " : " + message.Length + " chars");
+            Console.WriteLine(encodeMe + " (string to be encoded)");
+
+            //encode the string as a byte array
+            _messageBytes = Encoding.Unicode.GetBytes(encodeMe);
+
+            //sanity checking
+            Console.WriteLine(Encoding.Unicode.GetString(_messageBytes) + " (decoded bytes)");
+        }
+
+        //async Task ReceiveMessage()
+        //asyncronous recieve, waits for a message to be received
+        //accepts - nothing
+        //returns - nothing
+        async Task ReceiveMessage()
+        {
+            //wait for client to send message
+            UdpReceiveResult x = await client.ReceiveAsync();
+
+            byte[] newMsgBytes = new byte[x.Buffer.Length]; //byte array received from client
+
+            //copy received buffer array to new array (redundant)
+            for (int i = 0; i < x.Buffer.Length; ++i)
+            {
+                newMsgBytes[i] = x.Buffer[i];
+            }
+
+            //retrieve entire string from byte array
+            string newMessageString = Encoding.Unicode.GetString(newMsgBytes);
+
+            //regex to find instance of 2###-##-## ##:##:##
+            Regex datex = new Regex(@"([2-5][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9])");
+
+            //timestamp assumed formatted properly
+            string timeStamp = newMessageString.Substring(20, 19);
+            string name;
+            string message;
+            //if timestamp is formatted properly...
+            if (datex.IsMatch(timeStamp))
+            {
+                //proceed as usual
+                name = newMessageString.Substring(0, newMessageString.IndexOf(timeStamp));
+                name = name.Trim();
+                message = newMessageString.Substring(newMessageString.LastIndexOf(timeStamp) + timeStamp.Length);
+                if (message.Length > 250)
+                {
+                    message = message.Substring(0, 250).ToString();
+                }
+            }
+            else //otherwise...
+            {   //test entire message, if successful...
+                if (datex.IsMatch(newMessageString))
+                {
+                    //use timestamp to infer locations of name, message
+                    MatchCollection mDates = datex.Matches(newMessageString);
+                    timeStamp = mDates[0].ToString();
+                    name = newMessageString.Substring(0, newMessageString.IndexOf(timeStamp));
+                    name = name.Trim();
+                    message = newMessageString.Substring(newMessageString.LastIndexOf(timeStamp) + timeStamp.Length);
+                    if (message.Length > 250)
+                    {
+                        message = message.Substring(0, 250).ToString();
+                    }
+                }
+                else//otherwise...
+                {   //just try to find 2####-##-##
+                    Regex datex2 = new Regex(@"([2-5][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9])");
+                    //search entire message, if successful...
+                    if (datex2.IsMatch(newMessageString))
+                    {
+                        //use timestamp to infer locations of name, message
+                        MatchCollection mDates = datex2.Matches(newMessageString);
+                        timeStamp = mDates[0].ToString();
+                        name = newMessageString.Substring(0, newMessageString.IndexOf(timeStamp));
+                        name = name.Trim();
+                        message = newMessageString.Substring(newMessageString.LastIndexOf(timeStamp) + timeStamp.Length);
+                        if (message.Length > 250)
+                        {
+                            message = message.Substring(0, 250).ToString();
+                        }
+                    }
+                    else//otherwise...
+                    {   //search for ##/##/##
+                        Regex datex3 = new Regex(@"([0-9][0-9]-[0-9][0-9]-[0-9][0-9])");
+                        //search entire message, if successful...
+                        if (datex3.IsMatch(newMessageString))
+                        {
+                            //use timestamp to infer locations of name, message
+                            MatchCollection mDates = datex3.Matches(newMessageString);
+                            timeStamp = mDates[0].ToString();
+                            name = newMessageString.Substring(0, newMessageString.IndexOf(timeStamp));
+                            name = name.Trim();
+                            message = newMessageString.Substring(newMessageString.LastIndexOf(timeStamp) + timeStamp.Length);
+                            if (message.Length > 250)
+                            {
+                                message = message.Substring(0, 250).ToString();
+                            }
+                        }
+                        else//otherwise...
+                        {   //give up, use name (infered to be from index 0 -> first white space)
+                            name = newMessageString.Substring(0, newMessageString.IndexOf(' '));
+                            name = name.Trim();
+                            //indicate unknown time of sending...
+                            timeStamp = "unknown";
+                            message = newMessageString.Substring(name.Length);
+                            if (message.Length > 250)
+                            {
+                                message = message.Substring(0, 250).ToString();
+                            }
+                            //...but include time of receipt
+                            message += " [Received at: " + DateTime.Now.ToString() + "]";
+                        }
+                    }
+                }
+            }
+
+            //if address not included in friends list
+            if (!_friendsList.Contains(x.RemoteEndPoint.Address))
+            {
+                //add to friends list
+                _friendsList.Add(x.RemoteEndPoint.Address);
+                _names.Add(name);
+            }
+            //otherwise
+            else
+            {
+                //update contact username (in case they change it)
+                int index = _friendsList.IndexOf(x.RemoteEndPoint.Address);
+                if (_names[index] != name)
+                    _names[index] = name;
+            }
+            //update contact display
+            DisplayIPS();
+
+            //add newline
+            if (messageBoxMain.Text != "")
+            {
+                messageBoxMain.AppendText("\n");
+            }
+
+            //display new message in listview, color coded 
+            messageBoxMain.SelectionStart = messageBoxMain.Text.Length;
+            messageBoxMain.SelectionLength = 0;
+            messageBoxMain.SelectionColor = Color.Green;
+            messageBoxMain.AppendText($"[{timeStamp}]  ::  ");
+
+            messageBoxMain.SelectionStart = messageBoxMain.Text.Length;
+            messageBoxMain.SelectionLength = 0;
+            messageBoxMain.SelectionColor = Color.Red;
+            messageBoxMain.AppendText($"{name} Says : ");
+
+            messageBoxMain.SelectionStart = messageBoxMain.Text.Length;
+            messageBoxMain.SelectionLength = 0;
+            messageBoxMain.SelectionColor = Color.Blue;
+            messageBoxMain.AppendText(message);
+
+            //call method, wait for new message
+            _ = ReceiveMessage();
+        }
+
+        //async Task SendMessage()
+        //asyncronous send, sends user generated message to each IP in friends list
+        //accepts - nothing
+        //receives - nothing
+        async Task SendMessage()
+        {
+            //send message to each IP in friends list
+            foreach(IPAddress addy in _friendsList)
+            {
+                await client.SendAsync(_messageBytes, _messageBytes.Length, addy.ToString(), 1666);
+                Console.WriteLine(addy.ToString());
+            }
+        }
+
+        //occurs when a new IP is added to friends list
+        //updates DGV with new address
+        public void DisplayIPS()
+        {
+            //reset IP list
+            friendListView.Items.Clear();
+            for (int i = 0; i < _names.Count; i++)
+            {
+                ListViewItem item = new ListViewItem(_names[i]);
+                item.SubItems.Add(_friendsList[i].ToString());
+                friendListView.Items.Add(item);
+            }
+        }
+
+        //occurs when message is added to message display
+        //ensures that messagebox scrolls to bottom of text
+        private void MessageBoxMain_TextChanged(object sender, EventArgs e)
+        {
+            messageBoxMain.SelectionStart = messageBoxMain.Text.Length;
+            messageBoxMain.ScrollToCaret();
+        }
+    }
+}
